4cb2607e35a70d1100aa0d2b2c1791f827161187
Rovun <rovun@foxmail.com>
2014-01-16 11:31:26 +0800

    Update qqConnect.php

--- init/Extensions/qqConnect.php
+++ init/Extensions/qqConnect.php
@@ -1,8 +1,8 @@
 <?php
 /////////////////////////////////////////////////////////////////
-//云边开源轻博, Copyright (C)   2010 - 2011  qing.thinksaas.cn 
-//EMAIL:nxfte@qq.com QQ:234027573                              
-//$Id: qqConnect.php 589 2012-05-11 15:50:28Z anythink $ 
+//优云开源轻博, Copyright (C)   2010 - 2011  www.rovun.com 
+//EMAIL:rovun@foxmail.com     Q群:329354214                              
+//$Id: qqConnect.php 589 2012-05-11 15:50:28Z rovun $ 
 
 
 if( substr(PHP_VERSION, 0, 3) == "5.3" ){
