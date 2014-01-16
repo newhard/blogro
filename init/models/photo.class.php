963ff991f779519bc2f9f9c217d9eb2e9c79612f
Rovun <rovun@foxmail.com>
2014-01-16 11:42:25 +0800

    Update photo.class.php

--- init/models/photo.class.php
+++ init/models/photo.class.php
@@ -1,8 +1,8 @@
 <?php
 /////////////////////////////////////////////////////////////////
-//云边开源轻博, Copyright (C)   2010 - 2011  qing.thinksaas.cn 
-//EMAIL:nxfte@qq.com QQ:234027573                              
-//$Id: photo.class.php 1158 2012-07-01 14:30:28Z anythink $         
+//优云开源轻博, Copyright (C)   2010 - 2011  www.rovun.com 
+//EMAIL:rovun@foxmail.com     Q群:329354214                              
+//$Id: photo.class.php 1158 2012-07-01 14:30:28Z rovun $         
 
 
 class yb_photo extends basePostModel
