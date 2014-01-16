731540e273c7b24a01386a8075396ad00044c537
Rovun <rovun@foxmail.com>
2014-01-16 11:54:33 +0800

    Update db_feeds.php

--- model/db_feeds.php
+++ model/db_feeds.php
@@ -1,8 +1,8 @@
 <?php
 /////////////////////////////////////////////////////////////////
-//云边开源轻博, Copyright (C)   2010 - 2011  qing.thinksaas.cn 
-//EMAIL:nxfte@qq.com QQ:234027573                              
-//$Id: db_feeds.php 730 2012-06-06 13:29:57Z anythink $ 
+//优云开源轻博, Copyright (C)   2010 - 2011  www.rovun.com 
+//EMAIL:rovun@foxmail.com     Q群:329354214                              
+//$Id: db_feeds.php 730 2012-06-06 13:29:57Z rovun $ 
 
 
 class db_feeds extends ybModel  
