f74d1d756e9e7301428f4291e4806af2127b5237
Rovun <rovun@foxmail.com>
2014-01-16 11:23:44 +0800

    Update post.php

--- controller/post.php
+++ controller/post.php
@@ -1,8 +1,8 @@
 <?php
 /////////////////////////////////////////////////////////////////
-//云边开源轻博, Copyright (C)   2010 - 2011  qing.thinksaas.cn 
-//EMAIL:nxfte@qq.com QQ:234027573                              
-//$Id: post.php 858 2012-06-16 14:52:50Z anythink $         
+//优云开源轻博, Copyright (C)   2010 - 2011  www.rovun.com 
+//EMAIL:rovun@foxmail.com     Q群:329354214                              
+//$Id: post.php 858 2012-06-16 14:52:50Z rovun $         
 
 
 class post extends top
