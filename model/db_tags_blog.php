eb0523cbb6d3657a8d870b6b8bf35bfd72963a2d
Rovun <rovun@foxmail.com>
2014-01-16 12:14:33 +0800

    Update db_tags_blog.php

--- model/db_tags_blog.php
+++ model/db_tags_blog.php
@@ -1,8 +1,8 @@
 <?php
 /////////////////////////////////////////////////////////////////
-//云边开源轻博, Copyright (C)   2010 - 2011  qing.thinksaas.cn 
-//EMAIL:nxfte@qq.com QQ:234027573                              
-//$Id: db_tags_blog.php 1053 2012-06-28 14:13:28Z anythink $ 
+//优云开源轻博, Copyright (C)   2010 - 2011  www.rovun.com 
+//EMAIL:rovun@foxmail.com     Q群:329354214                              
+//$Id: db_tags_blog.php 1053 2012-06-28 14:13:28Z rovun $ 
 
 
 class db_tags_blog extends spModel  
