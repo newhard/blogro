5cdf0f17c871f2a99d5875bb5c2bfe5b48192725
Rovun <rovun@foxmail.com>
2014-01-16 11:56:22 +0800

    Update db_invite_friend.php

--- model/db_invite_friend.php
+++ model/db_invite_friend.php
@@ -1,8 +1,8 @@
 <?php
 
 /////////////////////////////////////////////////////////////////
-//云边开源轻博, Copyright (C)   2010 - 2011  qing.thinksaas.cn
-//EMAIL:zhangshaomin_1990@126.com QQ:1470506882
+//优云开源轻博, Copyright (C)   2010 - 2011  www.rovun.com 
+//EMAIL:rovun@foxmail.com     Q群:329354214
 //邀请过的好友模块数据模型
 
 class db_invite_friend extends ybModel
