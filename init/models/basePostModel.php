d054d46f2867143c4d49e093b7a5a4b9f484c6ac
Rovun <rovun@foxmail.com>
2014-01-16 11:38:34 +0800

    Update basePostModel.php

--- init/models/basePostModel.php
+++ init/models/basePostModel.php
@@ -1,8 +1,8 @@
 <?php
 /////////////////////////////////////////////////////////////////
-//云边开源轻博, Copyright (C)   2010 - 2011  qing.thinksaas.cn 
-//EMAIL:nxfte@qq.com QQ:234027573                              
-//$Id: basePostModel.php 1311 2012-07-24 12:47:48Z anythink $         
+//优云开源轻博, Copyright (C)   2010 - 2011  www.rovun.com 
+//EMAIL:rovun@foxmail.com     Q群:329354214                              
+//$Id: basePostModel.php 1311 2012-07-24 12:47:48Z rovun $         
 
 
 abstract class basePostModel extends top
