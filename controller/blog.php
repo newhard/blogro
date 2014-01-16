5daa7cdf729a02cf47f5ac760eb533dbbf5e9eb4
Rovun <rovun@foxmail.com>
2014-01-16 11:18:01 +0800

    Update blog.php

--- controller/blog.php
+++ controller/blog.php
@@ -1,8 +1,8 @@
 <?php
 /////////////////////////////////////////////////////////////////
-//云边开源轻博, Copyright (C)   2010 - 2011  qing.thinksaas.cn 
-//EMAIL:nxfte@qq.com QQ:234027573                              
-//$Id: blog.php 1334 2012-08-06 14:25:25Z anythink $ 
+//优云开源轻博, Copyright (C)   2010 - 2011  www.rovun.com 
+//EMAIL:rovun@foxmail.com     Q群:329354214                              
+//$Id: blog.php 1334 2012-08-06 14:25:25Z rovun $ 
 
 
 //博客动作执行
