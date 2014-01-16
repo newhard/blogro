513141699b7eda0c66419103c63de194d1c43d75
Rovun <rovun@foxmail.com>
2014-01-16 12:09:08 +0800

    Update db_replay.php

--- model/db_replay.php
+++ model/db_replay.php
@@ -1,8 +1,8 @@
 <?php
 /////////////////////////////////////////////////////////////////
-//云边开源轻博, Copyright (C)   2010 - 2011  qing.thinksaas.cn 
-//EMAIL:nxfte@qq.com QQ:234027573                              
-//$Id: db_replay.php 904 2012-06-17 13:50:09Z anythink $ 
+//优云开源轻博, Copyright (C)   2010 - 2011  www.rovun.com 
+//EMAIL:rovun@foxmail.com     Q群:329354214                              
+//$Id: db_replay.php 904 2012-06-17 13:50:09Z rovun $ 
 
 
 // 评论发布与管理
