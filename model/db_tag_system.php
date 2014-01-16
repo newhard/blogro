7d0dfc2d061b479f463604b97421e2dab90145bb
Rovun <rovun@foxmail.com>
2014-01-16 12:10:44 +0800

    Update db_tag_system.php

--- model/db_tag_system.php
+++ model/db_tag_system.php
@@ -1,8 +1,8 @@
 <?php
 /////////////////////////////////////////////////////////////////
-//云边开源轻博, Copyright (C)   2010 - 2011  qing.thinksaas.cn 
-//EMAIL:nxfte@qq.com QQ:234027573                              
-//$Id: db_skins.php 754 2012-06-10 07:57:09Z anythink $ 
+//优云开源轻博, Copyright (C)   2010 - 2011  www.rovun.com 
+//EMAIL:rovun@foxmail.com     Q群:329354214                              
+//$Id: db_skins.php 754 2012-06-10 07:57:09Z rovun $ 
 
 class db_tag_system extends ybModel  
 {  
