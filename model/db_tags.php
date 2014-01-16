ea688fc2abbd1aa006f1718712559af22c5261a0
Rovun <rovun@foxmail.com>
2014-01-16 12:12:08 +0800

    Update db_tags.php

--- model/db_tags.php
+++ model/db_tags.php
@@ -1,8 +1,8 @@
 <?php
 /////////////////////////////////////////////////////////////////
-//云边开源轻博, Copyright (C)   2010 - 2011  qing.thinksaas.cn 
-//EMAIL:nxfte@qq.com QQ:234027573                              
-//$Id: db_tags.php 1334 2012-08-06 14:25:25Z anythink $ 
+//优云开源轻博, Copyright (C)   2010 - 2011  www.rovun.com 
+//EMAIL:rovun@foxmail.com     Q群:329354214                              
+//$Id: db_tags.php 1334 2012-08-06 14:25:25Z rovun $ 
 
 class db_tags extends ybModel  
 {  
